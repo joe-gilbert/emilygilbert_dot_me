1543cc07ead5d35abf5423b2ac248499f630290c
joe-gilbert <JoeGilbert85@gmail.com>
2017-06-27 20:39:52 -0400

    Initial commit

--- header.php
+++ header.php
@@ -86,5 +86,25 @@
 	<div class="container <?php echo post_or_page_specific_class(); ?>">
 
 		<header class="sitewide-header">
-			<h1><a href="/" class="decorationless"><img src="<?php echo bloginfo('template_directory') ?>/assets/images/logos/emily-joy-gilbert-600x189.png" alt="Emily Joy Gilbert" class="site-logo"></a></h1>
+			<h1><a href="/" class="site-logo decorationless"><img src="<?php echo bloginfo('template_directory') ?>/assets/images/logos/emily-joy-gilbert-600x189.png" alt="Emily Joy Gilbert"></a></h1>
+			<nav>
+				<a href="#" class="hamburger-menu-control">
+					<div class="hamburger-container">
+						<span class="hamburger-bar hamburger-bar-top"></span>
+						<span class="hamburger-bar hamburger-bar-mid"></span>
+						<span class="hamburger-bar hamburger-bar-btm"></span>
+					</div>
+				</a>
+
+				<div class="navi-elements-container">
+					<ul class="navi-elements">
+						<li><a href="/">Home</a></li>
+						<li><a href="/about/">About</a></li>
+						<li><a href="/category/personal/">Personal</a></li>
+						<li><a href="/category/portraits/">Portraits</a></li>
+						<li><a href="/about/">Contact</a></li>
+						<li><a href="/pricing">Pricing</a></li>
+					</ul>
+				</div>
+			</nav>
 		</header>
--- source/js/functions.js
+++ source/js/functions.js
@@ -1,4 +1,55 @@
+/*********************************************
+		Functions
+*********************************************/
+function makeObjectFixedPositioned(nonFixedObject){
+
+	var objectToFix = $(nonFixedObject),
+			objectsPosition = objectToFix.offset();
+
+	objectToFix.css({
+		'position' : 'fixed',
+		'top' : objectsPosition.top,
+		'left' : objectsPosition.left
+	});
+
+} /* END function makeObjectFixedPositioned */
+
+
+function removeInlineStyles(styledObject){
+
+	var objectToFix = $(styledObject);
+
+	objectToFix.removeAttr('style');
+
+} /* END function makeObjectFixedPositioned */
+
+
+/*********************************************
+		jQuery Actions
+*********************************************/
 $(document).ready(function(){
+
+
+	/* Controls the top navigation click functionality */
+	$('.hamburger-menu-control').on('click', function(e){
+		/* Prevent the default anchor jump */
+		e.preventDefault();
+
+		/* Toggle the class 'active-navi' on the hamburger menu's parent nav */
+		$(this).parent('nav').toggleClass('active-navi');
+
+		/* Make the hamburger menu control fixed position to prevent jump when the scroll bar is visible. */
+		if( $('.active-navi').length ){
+			makeObjectFixedPositioned('.hamburger-menu-control');
+		}else{
+			removeInlineStyles('.hamburger-menu-control');
+		}
+
+		/* Prevent window scrolling */
+		$('html, body').toggleClass('overlay-no-scroll');
+
+	}); /* END. On .hamburger-menu-control click */
+
 
 	/* Single Post Masonry Plugin */
 	if( $('.post-gallery-grid').length ){
@@ -13,7 +64,8 @@ $(document).ready(function(){
 		$masonryGrid.imagesLoaded().progress( function() {
 			$masonryGrid.masonry('layout');
 		});
-	}
+	} /* END if .post-gallery-gird length */
+
 
 	/* Homepage Gallery Slider Functionality */
 	if( $('.gallery-slider').length ){
@@ -28,12 +80,15 @@ $(document).ready(function(){
 		  pauseControls: true,	// Boolean: Pause when hovering controls, true or false
 		  namespace: "slider"		// String: Change the default namespace used
 		});
-	}
+	} /* END if .gallery-slider length */
+
 
 }); /* END Document Ready Function */
 
 
-/* Google Analytics Tracking */
+/*********************************************
+		Google Analytics Tracking
+*********************************************/
 (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
 (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
 m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
